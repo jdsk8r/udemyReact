6bb44915bb487de104fead7eb4803297823d9b88
diego <diego.sanchez-rolfsen@outlook.com>
2021-11-14 13:09:24 +0100

    lesson 56

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,37 +2,40 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
-//   const [enteredTitle, setEnteredTitle] = useState('');
-//   const [enteredAmount, setEnteredAmount] = useState('');
-//   const [enteredDate, setEnteredDate] = useState('');
-const [userInput, setUserInput]  = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: ''
-});
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+//   const [userInput, setUserInput] = useState({
+//     enteredTitle: "",
+//     enteredAmount: "",
+//     enteredDate: "",
+//   });
 
   function titleChangeHandler(event) {
-    // setEnteredTitle(event.target.value);
-    setUserInput({
-        ...userInput,
-        enteredTitle: event.target.value
-    });
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredTitle: event.target.value
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   }
 
   function amountChangeHandler(event) {
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-        ...userInput,
-        enteredAmount: event.target.value
-    });
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   }
 
   function dateChangeHandler(event) {
-    // setEnteredDate(event.target.value);
-    setUserInput({
-        ...userInput,
-        enteredDate: event.target.value
-    });
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
   }
 
   return (
