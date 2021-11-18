57f3f07a5d7e0cd88225102a0f98f7a81fa288da
diego <diego.sanchez-rolfsen@outlook.com>
2021-11-18 18:20:03 +0100

    lesson 66

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import React, { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Chocolate",
+    amount: 239.63,
+    date: new Date(2021, 10, 31),
+  },
+  { id: "e2", title: "Milk", amount: 23.45, date: new Date(2021, 10, 28) },
+  {
+    id: "e3",
+    title: "Deodorant",
+    amount: 25.12,
+    date: new Date(2021, 10, 30),
+  },
+  {
+    id: "e4",
+    title: "Shoes",
+    amount: 500.12,
+    date: new Date(2021, 11, 1),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Chocolate",
-      amount: 239.63,
-      date: new Date(2021, 10, 31),
-    },
-    { id: "e2", title: "Milk", amount: 23.45, date: new Date(2021, 10, 28) },
-    {
-      id: "e3",
-      title: "Deodorant",
-      amount: 25.12,
-      date: new Date(2021, 10, 30),
-    },
-    {
-      id: "e4",
-      title: "Shoes",
-      amount: 500.12,
-      date: new Date(2021, 11, 1),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -43,7 +43,7 @@ function ExpenseForm(props) {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: parseInt(enteredAmount),
       date: new Date(enteredDate),
     };
 
